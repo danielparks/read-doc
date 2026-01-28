35b007118d39957d41b2facdd13282eef3d4c958
Daniel Parks <dp+git@oxidized.org>
2026-01-28 15:59:26 -0800

    `cargo generate --name combine_docs --lib --git ~/projects/danielparks-template.rs`.

---
+++ README.md
@@ -0,0 +1,30 @@
+# combine-docs
+
+[![docs.rs](https://img.shields.io/docsrs/combine-docs)][docs.rs]
+[![Crates.io](https://img.shields.io/crates/v/combine-docs)][crates.io]
+![Rust version 1.65+](https://img.shields.io/badge/Rust%20version-1.65%2B-success)
+
+Generated from template https://github.com/danielparks/danielparks-template.rs
+with [cargo-generate](https://github.com/ashleygwilliams/cargo-generate).
+
+## Development status
+
+This is in active development. I am open to [suggestions][issues].
+
+## License
+
+Unless otherwise noted, this project is dual-licensed under the Apache 2 and MIT
+licenses. You may choose to use either.
+
+  * [Apache License, Version 2.0](LICENSE-APACHE)
+  * [MIT license](LICENSE-MIT)
+
+### Contributions
+
+Unless you explicitly state otherwise, any contribution you submit as defined
+in the Apache 2.0 license shall be dual licensed as above, without any
+additional terms or conditions.
+
+[docs.rs]: https://docs.rs/combine-docs/latest/combine_docs/
+[crates.io]: https://crates.io/crates/combine-docs
+[issues]: https://github.com/danielparks/combine-docs/issues
---
+++ release.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+
+set -eo pipefail
+shopt -s extglob
+
+version=$1
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+
+awk-in-place () {
+  local tmpfile=$(mktemp)
+  local original="$1"
+  shift
+  cp "$original" "$tmpfile"
+  awk "$@" <"$tmpfile" >"$original"
+  rm "$tmpfile"
+}
+
+check-changes () {
+  if git ls-files --exclude-standard --other | grep . >/dev/null ; then
+    echo 'Found untracked files:' >&2
+    git ls-files --exclude-standard --other | sed -e 's/^/  /' >&2
+    echo >&2
+    echo 'Please commit changes before proceeding.' >&2
+    return 1
+  fi
+
+  git diff --color --exit-code HEAD || {
+    echo >&2
+    echo 'Please commit changes before proceeding.' >&2
+    return 1
+  }
+}
+
+crate-names () {
+  cargo metadata --format-version 1 --no-deps | jq -r '.packages[].name'
+}
+
+auto-pr () {
+  pr_url=$((gh pr view --json url,closed 2>/dev/null || true) \
+    | jq -r 'select(.closed | not) | .url')
+
+  if [[ "$pr_url" ]] ; then
+    echo "Found existing PR: $pr_url"
+    echo
+  else
+    # Create a PR
+    gh pr create --fill-verbose --title "$1"
+  fi
+
+  gh pr merge --disable-auto --delete-branch
+  sleep 3
+  gh pr checks --watch --fail-fast
+
+  git checkout main
+  git pull
+  git merge --ff-only "$branch_name"
+  git push origin HEAD
+
+  git branch -d "$branch_name"
+  git push origin --delete "$branch_name"
+}
+
+confirm () {
+  local prompt="$1"
+  local answer
+  read -n 1 -p "${prompt} [yN] " answer
+  case $answer in
+    [yY]*) echo ;; # Continue
+    *) echo ; echo Canceling. >&2 ; exit 1 ;;
+  esac
+}
+
+case $version in
+  +([0-9]).+([0-9]).+([0-9])*) ;; # Good
+  *) echo "Usage $0 VERSION" >&2 ; exit 1 ;;
+esac
+
+command -v gh &>/dev/null || {
+  echo "gh not installed (https://cli.github.com)" >&2
+  exit 1
+}
+
+command -v jq &>/dev/null || {
+  echo "jq not installed (https://jqlang.org)" >&2
+  exit 1
+}
+
+command -v parse-changelog &>/dev/null || {
+  echo "parse-changelog not installed (https://github.com/taiki-e/parse-changelog)" >&2
+  exit 1
+}
+
+if [[ "$branch_name" = main ]] ; then
+  git switch -c "release-$version"
+fi
+
+check-changes
+
+echo 'Making sure version is correct.'
+
+awk-in-place Cargo.toml '
+  /^version *=/ && !done {
+    sub(/"[0-9.]+"/, "\"'$version'\"")
+    done=1
+  }
+  { print }'
+
+# Fix docs.rs links in README, if present
+echo 'Updating links in README.md'
+for name in $(crate-names) ; do
+  awk-in-place README.md '{
+      sub(/https:\/\/docs\.rs\/'"$name"'\/[0-9]+.[0-9]+.[0-9]+\//, \
+        "https://docs.rs/'"$name"'/'$version'/")
+      print
+    }'
+done
+
+cargo check --quiet
+
+# Do semver checks only if there is a version on crates.io to compare to.
+# FIXME: can’t tell if crates.io search failed for another reason.
+main_crate_name=$(crate-names | head -1)
+if (cd / && cargo info "$main_crate_name" &>/dev/null) ; then
+  echo 'Doing semantic versioning checks'
+  cargo semver-checks || { echo ; confirm 'Release anyway?' ; }
+fi
+
+awk-in-place CHANGELOG.md '
+  /^## / && !done {
+    $0 = "## Release '$version' ('$(date +%Y-%m-%d)')"
+    done=1
+  }
+  { print }'
+
+# Confirm changelog
+changelog=$(mktemp)
+{
+  echo "## Release ${version}"
+  echo
+  parse-changelog CHANGELOG.md "$version"
+} >"$changelog"
+
+cat "$changelog"
+echo
+confirm 'Release notes displayed above. Continue?'
+
+# Commit version bump if necessary.
+check-changes &>/dev/null || {
+  git add -u
+  git commit --cleanup=verbatim --file - <<EOF
+Release ${version}
+
+$(parse-changelog CHANGELOG.md "$version")
+EOF
+}
+
+check-changes
+
+git tag --force --sign --file "$changelog" --cleanup=verbatim "v${version}"
+git push --force --tags origin HEAD
+auto-pr "Release ${version}"
+
+cargo publish
+
+awk-in-place CHANGELOG.md '
+  /^## Release/ && !done {
+    print "## main branch\n"
+    done=1
+  }
+  { print }'
+
+git switch -c post-release
+git add CHANGELOG.md
+git diff --staged
+
+confirm 'Commit with message "Prepping CHANGELOG.md for development."?'
+
+git commit -m 'Prepping CHANGELOG.md for development.'
+git push --force origin HEAD
+auto-pr "Prepping CHANGELOG.md for development"
